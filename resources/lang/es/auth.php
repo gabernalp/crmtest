0819c5c134ee536ec3b287e0d14509e1bebfb28b
Laravel Daily <info@laraveldaily.com>
2021-04-12 22:30:08 +0000

    QuickAdminPanel automatic commit

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Muchos intentos para ingresar. Por favor intente de nuevo en :seconds segundos',
+];
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Admin\CrmCustomerController;
+use App\Http\Controllers\Admin\CrmDocumentController;
+use App\Http\Controllers\Admin\CrmNoteController;
+use App\Http\Controllers\Admin\CrmStatusController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::redirect('/', '/login');
+
+Auth::routes(['register' => false]);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    // Permissions
+    Route::resource('permissions', PermissionController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Roles
+    Route::resource('roles', RoleController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Users
+    Route::resource('users', UserController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Crm Status
+    Route::resource('crm-statuses', CrmStatusController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Crm Customer
+    Route::resource('crm-customers', CrmCustomerController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Crm Note
+    Route::resource('crm-notes', CrmNoteController::class, ['except' => ['store', 'update', 'destroy']]);
+
+    // Crm Document
+    Route::post('crm-documents/media', [CrmDocumentController::class, 'storeMedia'])->name('crm-documents.storeMedia');
+    Route::resource('crm-documents', CrmDocumentController::class, ['except' => ['store', 'update', 'destroy']]);
+});
